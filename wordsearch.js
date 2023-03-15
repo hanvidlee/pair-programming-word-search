d34b5dda18b75bcb444b84c27917915f9e42e7ae
hanvidlee <hanvidlee@gmail.com>
2023-03-15 14:48:59 -0400

    wordsearch peer assignment

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word))
+      return true;
+  }
+
+  for (let i = 0; i < letters[0].length; i++) {
+    let verticalJoin = '';
+    for (let j = 0; j < letters.length; j++) {
+      verticalJoin += letters[j][i];
+    }
+    if (verticalJoin.includes(word)) {
+      return true;
+    }
+  };
+  return false;
+};
+
+
+module.exports = wordSearch;
+
+const printMatrix = (matrix) => {
+  for (const row of matrix) {
+    for (const el of row) {
+      process.stdout.write(el + " ");
+    }
+    process.stdout.write("\n");
+  }
+};
